1f8ba8cb4a45808eb0ba95a9297f53ac7d445b69
lorenzma <lorenzma@users.noreply.github.com>
2015-02-16 21:49:37 -0500

    Update lab2.js

--- lab2.js
+++ lab2.js
@@ -1,12 +1,21 @@
-
-var canvas;
 var gl;
 var points = [];
 var colors = [];
 
+var x;
+var y;
+var offset_x=0;
+var offset_y=0;
 
-var offset_x;
-var offset_y;
+var speed = 5;
+
+window.onload = function init()
+{
+    canvas = document.getElementById( "gl-canvas" );    
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }    
+	
+	//Declare vertices
 	var vertices = [
 		vec2(  0,  0),
 		vec2(  .05,  -0.1),
@@ -16,31 +25,20 @@ var offset_y;
 		vec2( -.1,0),
 		vec2( -.05,-0.1)
 	];
-
-window.onload = function init()
-{
-    canvas = document.getElementById( "gl-canvas" );    
-    gl = WebGLUtils.setupWebGL( canvas );
-    if ( !gl ) { alert( "WebGL isn't available" ); }    
-
-
-    
-    shape( vertices[0], vertices[1], vertices[2], vertices[3] , vertices[4] ,vertices[5] ,vertices[6], vertices[7]  );
 	
-    //
+	//Call function to build polygon
+    hexagon( vertices[0], vertices[1], vertices[2], vertices[3] , vertices[4] ,vertices[5] ,vertices[6], vertices[7]  );
+	
     //  Configure WebGL
-    //
     gl.viewport( 0, 0, canvas.width, canvas.height );
     gl.clearColor( 0, 0, 0, 1 );
         
     //  Load shaders and initialize attribute buffers
-    
     var program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
     
     // Create a buffer object, initialize it, and associate it with the
     //  associated attribute variable in our vertex shader
-    
     var cBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
     gl.bufferData( gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW );
@@ -55,81 +53,71 @@ window.onload = function init()
 
     var vPosition = gl.getAttribLocation( program, "vPosition" );
     gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
-
     gl.enableVertexAttribArray( vPosition );
+	
 	//event listeners for buttons
-
+	x = gl.getUniformLocation(program, "offset_x");
+	y = gl.getUniformLocation(program, "offset_y");
+	
 	window.addEventListener("keydown", function() {
 	 switch (event.keyCode) {
 		 case 65: // ’A’ key
-		 
-			offset_x=offset_x-1;
-			vertices[0]=vec2(0+offset_x,0);
-			alert(vertices[0]);
+			offset_x=offset_x-.01;
 		 break;
 		 case 83: // ’S’ key
-			offset_y=offset_y-1;
+			offset_y=offset_y-.01;
 		 break;
 		 case 68: // ’D’ key
-			offset_x=offset_xy+1;
+			offset_x=offset_x+.01;
 		 break;
 		 case 87: // ’W’ key
-			offset_y=offset_y+1;
+			offset_y=offset_y+.01;
 		 break;
-		 case 49: // ’1’ key
+		 case 49: // ’1’ key , resets to origin
 			 offset_x=0;
 			 offset_y=0;
 		 break;
 	 }
-	 
 
-	});
+	}, false);
 	
     render();
 };
-
-
-
 function triangle( a, b, c, color )
 {
-
     // add colors and vertices for one triangle
-
     var baseColors = [
-        vec3(1.0, 0.0, 0.0),
+        vec3(1.0, 0.0, 0.5),
         vec3(1, 1.0, 0.0),
-        vec3(0.0, 0.0, 1.0),
-        vec3(1.0, 1.0, 1.0)
+        vec3(0.0, 0.0, 1.0),    
     ];
-
+	//Puts points and colors into their designated arrays
     colors.push( baseColors[color] );
     points.push( a );
     colors.push( baseColors[color] );
     points.push( b );
     colors.push( baseColors[color] );
-    points.push( c );
-	
+    points.push( c );	
 }
-
-
-function shape( a, b, c, d, e, f,g )
+function hexagon( a, b, c, d, e, f,g )
 {
-    // tetrahedron with each side using
-    // a different color
-	
+//Six triangles form hexagon
     triangle( a, b, c, 0 );
     triangle( a, c, d, 1 );
     triangle( a, d, e, 2 );
 	triangle( a, e, f, 0 );
 	triangle( a, f, g, 1 );
 	triangle( a, g, b, 2 );
-	
 }
-
-
-
 function render()
 {
     gl.clear( gl.COLOR_BUFFER_BIT );
+	gl.uniform1f(x, offset_x);
+	gl.uniform1f(y, offset_y);
     gl.drawArrays( gl.TRIANGLES, 0, points.length );
+
+	setTimeout(
+        function () {requestAnimFrame( render );},
+        speed
+    );
 }
